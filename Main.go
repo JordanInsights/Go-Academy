7190d77ff0361073e46217a9a47dc2ce1816ca65
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-05-05 12:52:16 +0100

    Add capability to run day-3 code to Main.go

--- Main.go
+++ Main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	day1 "go-academy/day-1"
 	day2 "go-academy/day-2"
+	day3 "go-academy/day-3"
 )
 
 func main() {
@@ -18,6 +19,9 @@ func main() {
 	case 2:
 		fmt.Println("\nRunning the Day 2 code...")
 		day2.Day2()
+	case 3:
+		fmt.Println("\nRunning the Day 3 code...")
+		day3.Day3()
 	default:
 		fmt.Println("\nApologies, there's no code for the day you entered :(")
 	}
